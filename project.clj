f2714e5bb5c017e7af8489a4dbf90f5177a64772
alumi <alumi@users.noreply.github.com>
2017-12-11 18:42:23 +0900

    Upgrade midje to 1.9.0.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.3.465"]]
-  :profiles {:test {:dependencies [[midje "1.8.3"]]
+  :profiles {:test {:dependencies [[midje "1.9.0"]]
                     :plugins [[lein-midje "3.2.1"]]
                     :global-vars {*warn-on-reflection* true}
                     :jvm-opts ["-Xmx64m"]}}
